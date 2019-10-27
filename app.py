6ab7cdbb8f6bd0786c1b5efab83cabfa0e688e4d
Sergei <serhii.horodniuk@preen.me>
2019-10-27 23:41:05 +0200

    fix database

--- app.py
+++ app.py
@@ -8,57 +8,45 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ndwwwbpuuiwdli:087f70d386426f46deacd7da8a68df6454e345a1a9cb078b57bb586bf244e00a@ec2-107-21-200-103.compute-1.amazonaws.com:5432/d4bd6o61l2b9h7"
-app.config['SECRET_KEY'] = "8che9fj49fjsofjd93jfi59dje8b0e"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://pyaumkueyzideg:04138428c08de9af6e296214ba8f2cf43eb59b0bed0ad3d6bf83e06e667bc23c@ec2-107-21-126-201.compute-1.amazonaws.com:5432/d4btdruvjcdrq9"
+app.config['SECRET_KEY'] = "8ew9fyweuihwe8fwe8fwyefw"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    # by default tablename is 'user' (lower classname), but we change it
     __tablename__ = 'users'
-    # id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(45), unique=True, nullable=False)
-    created_at = db.Column(db.String(20))
+    email = db.Column(db.String(45), unique=True, primary_key=True, nullable=False)
     name = db.Column(db.String(45))
-    files = db.relationship('File', backref='user')
+    password = db.Column(db.String(100))
+    function = db.relationship('Functions', backref='user')
 
     def __repr__(self):
         return '<User %r>' % self.name
 
 
-class File(db.Model):
-    __tablename__ = 'files'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(45))
-    link = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    language_id = db.Column(db.Integer, db.ForeignKey('languages.id'))
-
-    def __repr__(self):
-        return '<FIle %r>' % self.id
-
-
-class Language(db.Model):
-    __tablename__ = 'languages'
+class Case(db.Model):
+    __tablename__ = 'cases'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(45))
     version = db.Column(db.String(20))
-    documentation_id = db.Column(db.Integer, db.ForeignKey('documentations.id'))
+    function_id = db.Column(db.Integer, db.ForeignKey('functions.id'))
 
     def __repr__(self):
-        return '<Language %r>' % self.name
+        return '<Case %r>' % self.name
 
 
-class Documentation(db.Model):
-    __tablename__ = 'documentations'
+class Funtion(db.Model):
+    __tablename__ = 'functions'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    link = db.Column(db.String(100))
-    actor = db.Column(db.String(45))
+    name = db.Column(db.String(100))
+    language = db.Column(db.String(40))
+    user_email = db.Column(db.String(100), db.ForeignKey('users.email'))
+
+    cases = db.relationship('Cases', backref='functions')
 
     def __repr__(self):
-        return '<Documentation %r>' % self.link
+        return '<Function %r>' % self.link
 
 
 @app.route('/', methods=['GET'])
